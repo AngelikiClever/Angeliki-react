d0df556cb12e6174b425af7c8d4ab9fbec2a0e06
Angeliki <angeliki@cleverads.com>
2021-01-17 14:15:46 +0100

    Update Forecast.js

--- src/Forecast.js
+++ src/Forecast.js
@@ -8,11 +8,13 @@ export default function Forecast() {
 <div className="d-flex justify-content-around">
         <div className="row" id="forecast">
           <div className="col">
-            <div className="card border-info mb-3" style="max-width: 11rem;">
+            <div className="card border-info mb-3" >
               <div className="card-header"></div>
               <div className="card-body text-info">
                 <img className="forecast-icon" alt="icon"/>
                 <div className="forecast-temperature">
+                    
+  
                   <strong></strong>
                 </div>
               </div>
